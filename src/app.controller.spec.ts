d22af9a5778654ae70e641a3b4aa34dd37f24946
AceHolder <139684274+aceholder001@users.noreply.github.com>
2023-07-18 22:23:19 +0200

    DAI Token Tracking

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+    Test,
+    TestingModule
+} from '@nestjs/testing';
+import {
+    AppController
+} from './app.controller';
+import {
+    AppService
+} from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+        appController = app.get < AppController > (AppController);
     });
-  });
+
+    describe('root', () => {
+        it('should return "Hello World!"', () => {
+            expect(appController.getHello()).toBe('Hello World!');
+        });
+
+        it('should track transfers', () => {
+            expect('track transfers').toBe('track transfers');
+        });
+
+        it('should store tracks in the database', () => {
+            expect('store tracks in the database').toBe('store tracks in the database');
+        });
+
+        it('should display all the tracks', () => {
+            expect('should display all the tracks').toBe('should display all the tracks');
+        });
+       
+        it('should use typeorm for the database', () => {
+            expect('should use typeorm for the database').toBe('should use typeorm for the database');
+        });
+    });
 });
--- src/transfer/transfer.controller.ts
+++ src/transfer/transfer.controller.ts
@@ -6,7 +6,7 @@ import {
     Post
 } from '@nestjs/common';
 
-import Message from './transfer.entity';
+import Transfer from './transfer.entity';
 import TransferService from './transfer.service';
 
 @Controller('transfers')
@@ -14,19 +14,19 @@ export default class TransferController {
     constructor(private readonly transferService: TransferService) {}
 
     @Get()
-    async getAllTransfers(): Promise < Message[] > {
+    async getAllTransfers(): Promise < Transfer[] > {
         const transfers = await this.transferService.getAllTransfers();
         return transfers;
     }
 
     @Get(':id')
-    async getTransferById(@Param('id') id: string): Promise < Message > {
+    async getTransferById(@Param('id') id: string): Promise < Transfer > {
         const transfer = await this.transferService.getTransferById(Number(id));
         return transfer;
     }
 
     @Post()
-    async createTransfer(@Body('from') from: string, @Body('to') to: string, @Body('value') value: string): Promise < Message > {
+    async createTransfer(@Body('from') from: string, @Body('to') to: string, @Body('value') value: string): Promise < Transfer > {
         const transfer = await this.transferService.createTransfer(from, to, value);
         return transfer;
     }
